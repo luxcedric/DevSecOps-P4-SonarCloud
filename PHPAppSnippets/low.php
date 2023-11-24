f14dfb3170a6b1b0cb375b41c29818d7e365059a
luxcedric <98240622+luxcedric@users.noreply.github.com>
2023-11-24 11:53:17 +0100

    Update low.php

--- PHPAppSnippets/low.php
+++ PHPAppSnippets/low.php
@@ -9,9 +9,15 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = md5( $pass );
 
 	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	$query  = "SELECT * FROM `users` WHERE user = :user AND password = :pass;";
+	$stmt = $this->conn->prepare($query);
+        $stmt->bind_param(":user", $user);
+        $stmt->bind_param(":pass", $pass);
+	$stmt->execute();
+	//$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
+	$result = $stmt->store_result();
+	
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
 		$row    = mysqli_fetch_assoc( $result );
