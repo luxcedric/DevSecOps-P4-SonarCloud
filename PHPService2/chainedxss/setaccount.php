5320f9e4e0679fc8ef0c4904b90d87514ba1736c
luxcedric <98240622+luxcedric@users.noreply.github.com>
2023-11-24 11:05:52 +0100

    Update setaccount.php

--- PHPService2/chainedxss/setaccount.php
+++ PHPService2/chainedxss/setaccount.php
@@ -1,11 +1,11 @@
 <?php
 header('Location: index.htm');
-require('../template/head.php');
+require_once('../template/head.php');
 $q = $_GET['q'];
 
 print <<<END
  Password Changed, redirecting you home.
 END;
 
-require('../template/foot.php');
+require_once('../template/foot.php');
 ?>
